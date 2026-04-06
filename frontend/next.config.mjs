3a07d8d87f32022e107e19746c1aab6789107968
Manuel Tellez <josemanueltellezangulo@gmail.com>
2026-04-06 15:06:25 -0400

    Actualizando lo que llega de develop

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,34 +3,34 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
       },
       //pare permitir cargar las imágenes desde Supabase Storage
       {
-        protocol: 'https',
-        hostname: 'yiwjlbpbziydpkowvfmd.supabase.co',
-        port: '',
-        pathname: '/storage/v1/object/**'
+        protocol: "https",
+        hostname: "yiwjlbpbziydpkowvfmd.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/**",
       },
       // para permitir imágenes externas de banderas
       {
-        protocol: 'https',
-        hostname: 'flagcdn.com',
-        port: '',
-        pathname: '/**'
+        protocol: "https",
+        hostname: "flagcdn.com",
+        port: "",
+        pathname: "/**",
       },
       // para permitir imágenes de Unsplash
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**'
-      }
-    ]
-  }
-}
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
